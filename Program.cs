38fd4784a812e35aad892427190324c06f6d6317
Qunexx <YtrikPlay@gmail.com>
2023-03-20 00:06:22 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using lib;
+
+namespace _1._5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("The 1.1 lab!");
+            Console.WriteLine("17 вариант");
+            Console.WriteLine("L=(2b^2/6)+2*(c^2-a^2)");
+            Console.WriteLine("Введите a,b,c");
+            double a, b, c;
+            Console.Write("a=");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("b=");
+            b = Convert.ToDouble(Console.ReadLine());
+            Console.Write("c=");
+            c = Convert.ToDouble(Console.ReadLine());
+            Mylib.linfunc(a, b, c);
+            Console.WriteLine("L= " + Mylib.linfunc(a, b, c));
+            Console.ReadKey();
+
+            Console.WriteLine("----------------------------");
+            Console.WriteLine("The 1.2 lab!");
+            Console.WriteLine("17 вариант");
+            Console.WriteLine("p=min(x^2,y)-max(y,z)/2");
+            double x, y, z;
+            Console.WriteLine("Введите x,y,z");
+            Console.Write("x= ");
+            x = Convert.ToDouble(Console.ReadLine());
+            Console.Write("y= ");
+            y = Convert.ToDouble(Console.ReadLine());
+            Console.Write("z= ");
+            z = Convert.ToDouble(Console.ReadLine());
+            Mylib.razn_minmax(x, y, z);
+            Console.WriteLine("p= " + Mylib.razn_minmax(x, y, z));
+
+            Console.WriteLine("----------------------------");
+            Console.WriteLine("The 1.3 lab!");
+            Console.WriteLine("1 вариант");
+            Console.WriteLine("f=sin(x)");
+            double xn, xk, h, f;
+            Console.WriteLine("Рекомендуемые значения xn = 0, xk = 1");
+            Console.Write("xn=");
+            xn = double.Parse(Console.ReadLine());
+            Console.Write("xk=");
+            xk = double.Parse(Console.ReadLine());
+            h = (xk - xn) / 10;
+            for (int i = 0; i <= 10; i++)
+            {
+                x = xn + i * h;
+                Console.WriteLine("| {0,7:0.00000 } | {1,8:0.00000} |", x, Mylib.MathF1(x));
+
+            }
+            Console.ReadKey();
+            Console.WriteLine("ряд от 0 до N (-1)^n * x^2n+1/(2n+1)!");
+            double S;
+            int n, N;
+            xk = 1;
+            xn = 0.1;
+            h = (xk - xn) / 10;
+            N = 16;
+            for (x = xn; x <= xk; x += h) //Цикл задающий изменение x от xn до xk с шагом h
+            {
+                
+                S = 1;
+                
+                for (n = 1; n <= N; n++)
+                {
+                    
+                    f=Mylib.Fact(n);
+
+                    S = S + Math.Pow(-1, n) * Math.Pow(x, 2 * n + 1) / f;
+                }
+      
+                Console.WriteLine("| {0,7:0.00000 } | {1,8:0.00000} |", x, S);
+
+
+
+            }
+            Console.ReadKey();
+
+            Console.WriteLine("----------------------------");
+            Console.WriteLine("The 1.4 lab!");
+            Console.WriteLine("Найти номера тех элементов массива, которые больше своего правого соседа, и количество таких элементов. ");
+            Console.Write("Введите размерность массива n = ");
+            int m = Convert.ToInt32(Console.ReadLine());
+            int[] mas = new int[m];
+            Mylib.MassivInput(m, mas);
+            Mylib.MassivOutput(m, mas);
+            Mylib.MassivCheck(m, mas);
+            
+        }
+    }
+}
---
+++ lib.cs
@@ -0,0 +1,119 @@
+﻿using System;
+
+namespace lib
+{
+    public static class Mylib
+    {
+        public static double linfunc(double a, double b, double c) //Считает линейную функцию
+        {
+         double res = ((2 * b * b / 6) + 2 * (c * c) - (a * a));
+         return res;
+        }
+
+        public static double razn_minmax(double x, double y, double z) //Находит минимальное и максимальное значение
+        {
+        double min, max;
+        if ((x * x) > y) min = y;
+        else { min = x * x; }
+        if (y > z) max = y;
+        else { max = z; }
+        double p = (min - max) / 2;
+        return p;
+        }
+
+
+        public static double MathF1(double x)  // Считает синус от функции
+        {
+         double f = Math.Sin(x);
+         return f;
+        }
+        public static void MathF11(double xn,double xk) 
+        {
+            double x, h, f;
+            
+            h = (xk - xn) / 10;
+            for (int i = 0; i <= 10; i++)
+            {
+                x = xn + i * h;
+                f = Math.Sin(x);
+                Console.WriteLine("При x = {0}, sin(x) = {1}", x, f);
+            }
+        }
+
+
+       
+        public static double Fact(int n)
+        {
+            double f = 1;
+            for (int i = 1; i <= 2 * n + 1; i++) //Вычисление факториала (2*n+1)!
+            { f = f * i; }
+            return f; 
+        }
+
+
+
+
+    public static void MassivInput(int m, int[] mas)
+    {
+            Console.WriteLine("------------------------");
+
+            for (int i = 0; i < m; i++)
+        {
+            Console.Write("Введите элемент массива mas[{0}] = ", i);
+            mas[i] = Convert.ToInt32(Console.ReadLine());
+        }
+            Console.WriteLine("------------------------");
+        }
+
+    public static void MassivOutput(int m,int[] mas)
+    {
+        for (int i = 0; i < m; i++)
+        {
+            Console.WriteLine("Элемент массива mas[{0}] = {1}", i, mas[i]);
+        }
+            Console.WriteLine("------------------------");
+    }
+
+    public static void MassivCheck(int m,int[] mas)
+    {
+        int k = 0;
+        for (int i = 0; i < m - 1; i++)
+        {
+            
+            if (mas[i] > mas[i + 1]) { Console.WriteLine("Элемент mas[{0}]>mas[{1}] | {2} > {3}", i, i + 1, mas[i], mas[i+1]); k++; }
+                
+
+
+        }
+            Console.WriteLine("------------------------");
+            if (k == 0) { Console.WriteLine("Нет элементов, удовлетворяющих условию => k = 0");}
+            else { Console.WriteLine("Кол-во элементов удовлетворяющих условию = " + k); }
+        
+    }
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
